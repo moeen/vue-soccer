d4e1310f5af952fbb29f46950ed200b567556048
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-04 19:50:30 +0000

    Deploy moeen/vue-soccer to github.com/moeen/vue-soccer.git:gh-pages

---
+++ precache-manifest.6dd98c7635780de64e94b1a95053edce.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f68d602f58e336c6c2b",
+    "url": "/css/app.90789945.css"
+  },
+  {
+    "revision": "4f68d602f58e336c6c2b",
+    "url": "/js/app.ea4324ad.js"
+  },
+  {
+    "revision": "1283a923e728d92b8e1d",
+    "url": "/js/chunk-vendors.1d353483.js"
+  },
+  {
+    "revision": "2c8b4a7c13f93fe48effe00d82bb9ab4",
+    "url": "/fonts/Vazir-FD.2c8b4a7c.woff2"
+  },
+  {
+    "revision": "1a983852626e3046670ee8f6d22ef659",
+    "url": "/fonts/Vazir-Bold-FD.1a983852.woff2"
+  },
+  {
+    "revision": "55ae7bbc2cf5990d2983d6f767dabbef",
+    "url": "/fonts/Vazir-Thin-FD.55ae7bbc.woff2"
+  },
+  {
+    "revision": "7d8ab911e7565d1605fc2fcdd1237936",
+    "url": "/fonts/Vazir-FD.7d8ab911.woff"
+  },
+  {
+    "revision": "e2b6c5678b437b77a1891d9018d530d5",
+    "url": "/fonts/Vazir-Black-FD.e2b6c567.woff2"
+  },
+  {
+    "revision": "7117db85bd1c09f66582859fb7c4384e",
+    "url": "/fonts/Vazir-Light-FD.7117db85.woff2"
+  },
+  {
+    "revision": "82a4ae1b59f9e49755ce50ab193a669f",
+    "url": "/fonts/Vazir-Bold-FD.82a4ae1b.woff"
+  },
+  {
+    "revision": "6bcfad8944671fd6c6fe69a428ac89b0",
+    "url": "/fonts/Vazir-Black-FD.6bcfad89.woff"
+  },
+  {
+    "revision": "4968dc8cf63fe6f3aed659cce0af220d",
+    "url": "/fonts/Vazir-Medium-FD.4968dc8c.woff2"
+  },
+  {
+    "revision": "06f5193d8e0d85d42fda9a4443a77f8b",
+    "url": "/fonts/Vazir-Thin-FD.06f5193d.woff"
+  },
+  {
+    "revision": "14c3b8dd022951402c66c00fff97dfdf",
+    "url": "/fonts/Vazir-Medium-FD.14c3b8dd.woff"
+  },
+  {
+    "revision": "da89bfc9835d263c609453edfea9762d",
+    "url": "/fonts/Vazir-Light-FD.da89bfc9.woff"
+  },
+  {
+    "revision": "4803d94edb20aa7a3780f513d8705e3e",
+    "url": "/fonts/Vazir-FD.4803d94e.eot"
+  },
+  {
+    "revision": "da57bb4b22aba2109179c0f840d4aea0",
+    "url": "/fonts/Vazir-FD.da57bb4b.ttf"
+  },
+  {
+    "revision": "d608f9f7d98b07832f7215156de0ca64",
+    "url": "/fonts/Vazir-Bold-FD.d608f9f7.ttf"
+  },
+  {
+    "revision": "dfba5e5e58aa5688c85f2e49936cab65",
+    "url": "/fonts/Vazir-Thin-FD.dfba5e5e.eot"
+  },
+  {
+    "revision": "6a479c95561c8d64b1e8d1008ae648a8",
+    "url": "/fonts/Vazir-Thin-FD.6a479c95.ttf"
+  },
+  {
+    "revision": "f2f16a19389d0f4d79bb4ed84a03c827",
+    "url": "/fonts/Vazir-Bold-FD.f2f16a19.eot"
+  },
+  {
+    "revision": "7567dbd87cdf00afc574c2383c52e708",
+    "url": "/fonts/Vazir-Black-FD.7567dbd8.eot"
+  },
+  {
+    "revision": "93f4a3833d49aeee8486d8a8326451ef",
+    "url": "/fonts/Vazir-Black-FD.93f4a383.ttf"
+  },
+  {
+    "revision": "4d6ab3b9477f8e984d4a8b6476db6294",
+    "url": "/fonts/Vazir-Light-FD.4d6ab3b9.eot"
+  },
+  {
+    "revision": "861b58685ceed77cadc3a2aa4fbcba35",
+    "url": "/fonts/Vazir-Light-FD.861b5868.ttf"
+  },
+  {
+    "revision": "987d8e565f93935bb3171862940a1112",
+    "url": "/fonts/Vazir-Medium-FD.987d8e56.eot"
+  },
+  {
+    "revision": "bf5fb21ba1b9e53ab74fcb9808588425",
+    "url": "/fonts/Vazir-Medium-FD.bf5fb21b.ttf"
+  },
+  {
+    "revision": "55d9209431ead995c1d192a6e05d03b8",
+    "url": "/img/logo.55d92094.png"
+  },
+  {
+    "revision": "0e548afe349444e1b2ff5a5db2092847",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
